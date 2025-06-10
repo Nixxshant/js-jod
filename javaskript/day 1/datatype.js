9cb1d4506ba91b53f53cc4be2befe621b32d9db3
Nixxshant <reddinishant60534@gmail.com>
2025-06-10 20:12:50 +0000

    1st commit

---
+++ javaskript/day 1/datatype.js
@@ -0,0 +1,16 @@
+"use strict" // treats all  js code as newer version
+
+let name= "nishant"
+let age= 20
+let gender="male"
+let isgay= "False"
+
+console.log(typeof name)
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
---
+++ javaskript/day 1/variable.js
@@ -0,0 +1,14 @@
+const acc_id = 1235
+let accEmail="nishu@gmail.com"
+var accName="nishu"
+accCity="raipur"// this is also a way to assign a variable
+/*
+let is a better option to use to assign a variable rather than using "var"
+*/
+
+
+// acc_id= 1111
+console.log(accEmail)
+console.log(accName)
+console.log(accCity);
+console.table([acc_id,accEmail,accName,accCity])
