1a71ef02c0f0554037c962b84576d7452b267907
Nixxshant <reddinishant60534@gmail.com>
2025-06-10 21:46:27 +0000

    d1 commit

---
+++ javaskript/day1/ops.js
@@ -0,0 +1,48 @@
+//let v= 69
+//let v1= -v
+
+/*
+console.log(v1);
+console.log(2+2);
+console.log(2-2);
+console.log(3*4);
+console.log(5/9);
+console.log(2**5);
+*/
+
+let a= true
+console.log(typeof a);
+
+let b= "apple"
+let c= a*b
+
+console.log(c);
+
+
+console.log("1"+2+2);
+console.log(1+5+"2"); // pehle addition hoga the it'll attach to the string
+console.log((((2+3)*5)/9))
+
+
+console.log(+true);
+
+let jod= 69
+jod++  // increments by 1
+console.log(jod);
+
+jod--
+console.log(jod);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
