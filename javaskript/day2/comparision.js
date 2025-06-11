f2cdf708fad622de1c935730d0e83a45c2ee2202
NISHANT RAMAREDDI <reddinishant60534@gmail.com>
2025-06-11 11:46:42 +0000

    1st

---
+++ javaskript/day2/comparision.js
@@ -0,0 +1,29 @@
+// let a =10
+// let b =20
+// console.log(a>b);
+
+// console.log(2>2)
+// console.log(2<2)
+// console.log(2>=2)
+// console.log(2==2)
+// console.log(23>=23);
+// console.log(2!=22)
+// console.log("2">=2)
+
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null!=0)
+// console.log(NaN !=0);
+
+// console.log(undefined!=0)
+
+// console.log("2"===2) //=== checks datatype also
+
+// console.log("24"==="24");
+
+
+const a= Symbol("123")
+const b= Symbol("123")
+
+console.log(a==b);
+console.log(a===b);
