c540d59acc30f3c10c54f642f90728bd34f2c9bb
NISHANT RAMAREDDI <reddinishant60534@gmail.com>
2025-06-11 16:33:54 +0000

    10pm

---
+++ javaskript/day2/nonprimitive.js
@@ -0,0 +1,44 @@
+//non primitive dtypes: array, object and function
+
+//array
+const heroes=["thor","black panther","hulk"]
+console.log(heroes);
+
+
+//object
+let a={
+    name:"Nishu",
+    age:"20"
+}
+console.log(a);
+
+
+//functions
+const Fn = function(){
+    console.log("hello i am the goat");
+    }
+console.log(a);
+
+let z= 100000000000000025424242525252523434234n
+
+console.log(typeof z)
+console.log(typeof a);
+console.log(typeof Fn)
+
+
+//type of return 
+
+// Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+//non-primitive
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
+
