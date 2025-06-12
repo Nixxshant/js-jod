d4e3857f428c154bc43d05a3417ba32e2e657809
NISHANT RAMAREDDI <reddinishant60534@gmail.com>
2025-06-12 07:14:54 +0000

    tbc

---
+++ javaskript/day2/string.js
@@ -0,0 +1,5 @@
+//string interpolation
+const a="apple"
+const b="banana"
+
+console.log(`red coloured fruit is ${a} and yellow coloured fruit is ${b}`); // single quotes nhi hai, wo backticks(``) hai
